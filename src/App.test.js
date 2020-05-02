e4d9734bf1ee1f016fe8109200b83c1b682288db
pradeep misra <pradeep.misra@kognitiv.com>
2020-05-03 02:30:14 +0530

    updated test case for app component

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders welcome message', () => {
   const { getByText } = render(<App />);
   const textElement = getByText('Welcome to React Demo App2 !');
   expect(textElement).toBeInTheDocument();
